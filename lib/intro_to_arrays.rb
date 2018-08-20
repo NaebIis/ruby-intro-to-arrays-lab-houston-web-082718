c3e9e45dd317095b64b839c2dd067eb591159e65
John Grubbs <johnmlowery@gmail.com>
2018-08-20 14:35:26 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,7 +14,7 @@ def first_element(array)
 end
 
 def third_element(array)
-newArray2 = ["Welcome to New York", 'one', "style"]
+newArray2 = ["Welcome to New York", 'one', "Style"]
  return newArray2[2]
  end
   
